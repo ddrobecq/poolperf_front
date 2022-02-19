fd97921e8500b4ad7ac9eed288670f1e0555acc6
ddrobecq <didier@drobecq.fr>
2022-02-19 14:28:06 +0100

    stats

--- javascripts/game.js
+++ javascripts/game.js
@@ -1,17 +1,17 @@
-var player1 = {playerId: 1, nbShot: 0, nbPocket: 0, nbFault: 0};
-var player2 = {playerId: 2, nbShot: 0, nbPocket: 0, nbFault: 0};
+var player1 = {playerId: 1, nbShot: 0, nbPocket: 0, nbFoul: 0};
+var player2 = {playerId: 2, nbShot: 0, nbPocket: 0, nbFoul: 0};
 
 /* CUSTOMIZE THE PAGE */
-function composePagePool () {
+function composePageGame () {
 
     //SET TITLE
     setTitle (getParam ('title'));
 
     //SET USERS'S NAMES
-    $.get (APIURL + "/user/" + player1.playerId, function (data) {
+    $.get (APIURL + "/users/" + player1.playerId, function (data) {
         document.getElementById ("btnUser1").innerHTML = data[0].usr_name;
     }, "json");
-    $.get (APIURL + "/user/" + player2.playerId, function (data) {
+    $.get (APIURL + "/users/" + player2.playerId, function (data) {
         document.getElementById ("btnUser2").innerHTML = data[0].usr_name;
     }, "json");
 }
@@ -23,16 +23,16 @@ function shotCount (typeShot, numPlayer) {
             if (numPlayer == 1) {
                 player1.nbShot++;
                 document.querySelector("#btnPocket1").disabled = false;
-                document.querySelector("#btnFault1").disabled = false;
+                document.querySelector("#btnFoul1").disabled = false;
                 document.querySelector("#btnPocket2").disabled = true;
-                document.querySelector("#btnFault2").disabled = true;
+                document.querySelector("#btnFoul2").disabled = true;
             }
             else {
                 player2.nbShot++;
                 document.querySelector("#btnPocket1").disabled = true;
-                document.querySelector("#btnFault1").disabled = true;
+                document.querySelector("#btnFoul1").disabled = true;
                 document.querySelector("#btnPocket2").disabled = false;
-                document.querySelector("#btnFault2").disabled = false;
+                document.querySelector("#btnFoul2").disabled = false;
             }
             
             break;
@@ -42,28 +42,32 @@ function shotCount (typeShot, numPlayer) {
             else
                 player2.nbPocket++;
             break;
-        case "fault" :
+        case "Foul" :
             if (numPlayer == 1)
-                player1.nbFault++;
+                player1.nbFoul++;
             else
-                player2.nbFault++;
+                player2.nbFoul++;
             break;
     }
 
-    document.getElementById ("txtShot1").innerHTML = "Nbre de Coups : " + player1.nbShot;
-    document.getElementById ("txtShot2").innerHTML = "Nbre de Coups : " + player2.nbShot;
-    document.getElementById ("txtPocket1").innerHTML = "Billes empochées : " + player1.nbPocket + " " + (player1.nbPocket/player1.nbShot * 100).toFixed(0) + "%";
-    document.getElementById ("txtPocket2").innerHTML = "Billes empochées : " + player2.nbPocket + " " + (player2.nbPocket/player2.nbShot * 100).toFixed(0) + "%";
-    document.getElementById ("txtFault1").innerHTML = "Nbre de fautes : " + player1.nbFault + " " + (player1.nbFault/player1.nbShot * 100).toFixed(0) + "%";
-    document.getElementById ("txtFault2").innerHTML = "Nbre de fautes : " + player2.nbFault + " " + (player2.nbFault/player2.nbShot * 100).toFixed(0) + "%";
+    document.querySelector("#btnShot1").textContent = "Coups : " + player1.nbShot;
+    document.querySelector("#btnShot2").textContent = "Coups : " + player2.nbShot;
+    document.querySelector("#btnPocket1").textContent = "Empoches : " + player1.nbPocket + " " + (player1.nbPocket/player1.nbShot * 100).toFixed(0) + "%";
+    document.querySelector("#btnPocket2").textContent = "Empoches : " + player2.nbPocket + " " + (player2.nbPocket/player2.nbShot * 100).toFixed(0) + "%";
+    document.querySelector("#btnFoul1").textContent = "Fautes : " + player1.nbFoul + " " + (player1.nbFoul/player1.nbShot * 100).toFixed(0) + "%";
+    document.querySelector("#btnFoul1").textContent = "Fautes : " + player2.nbFoul + " " + (player2.nbFoul/player2.nbShot * 100).toFixed(0) + "%";
 }
 
 /* INSERT GAME's SCORE INTO THE DATABASE */
 function gameSave(player1, player2) {
     console.log (player1);
-    $.post(APIURL + "/game", { 
+    $.post(APIURL + "/games", { 
         gameType: getParam ('title'),
-        player1: JSON.stringify(player1),
-        player2: JSON.stringify (player2)
+        player: JSON.stringify(player1)
+    }, {},"json");
+    console.log (player2);
+    $.post(APIURL + "/games", { 
+        gameType: getParam ('title'),
+        player: JSON.stringify (player2)
     }, {},"json");
 }
--- javascripts/user.js
+++ javascripts/user.js
@@ -1,14 +1,20 @@
+var player = {usr_id: 1, usr_name: "", usr_avatar: null};
 
 /* GET user INFO FROM DATABASE */
-function userGetOne (usrId, target) {
+function userGetOne (usrId) {
     let request = new XMLHttpRequest();
 
-    request.open ("GET", APIURL + "/user/" + usrId);
+    request.open ("GET", APIURL + "/users/" + usrId);
     request.responseType = "json";
     request.onload = function () {
-        target = request.response[0].usr_name;
+        //return (request.response);
     };
-    request.send ();
+    return new Promise(resolve => {
+        request.send ();
+        res = request.response;
+        console.log (res);
+        resolve (res);
+      });
 /*
     console.log ("get user info");
 
@@ -21,20 +27,36 @@ function userGetOne (usrId, target) {
 /* UPDATE CHANGES TO DATABASE */
 function userSave() {
     let strName = document.getElementById("inName").value;
-    let jsonData = {"usr_name":strName};
- 
-    $.ajax ({
+     $.ajax ({
         method: "PUT",
-        url: APIURL + "/user/" + getParam ('usr_id'), 
+        url: APIURL + "/users/" + player.usr_id, 
         data: {
             usr_name: strName
         }
-    }, function (data) {
-        console.log ("UPDATE PUTED!");
-    }, "json");
+    }, {}, "json");
 }
 
 /* ENABLE BUTTON TO SAVE CHANGES (IF THERE IS ANY) */
 function enableSave() {
     document.querySelector("#btnSave").disabled = false;
+}
+
+function composePageUser (usrId) {
+    /* GET USER INFO TO FULLFILL THE FORM */
+    $.get (APIURL + "/users/" + usrId, function (data) {
+        player.usr_id = usrId;
+        player.usr_name = data[0].usr_name;
+        player.usr_avatar = data[0].usr_avatar;
+        document.getElementById("inName").value = player.usr_name;
+    }, "json");
+
+    /* GET USER'S AVERAGE TO FULLFILL THE TABLE */
+    $.get (APIURL + "/users/" + usrId + "/stats", function (data) {
+        document.getElementById("avgPocket").innerHTML = (data[0].avgPocket * 100).toFixed(0) + "%";
+        document.getElementById("minPocket").innerHTML = (data[0].minPocket * 100).toFixed(0) + "%";
+        document.getElementById("maxPocket").innerHTML = (data[0].maxPocket * 100).toFixed(0) + "%";
+        document.getElementById("avgFoul").innerHTML = (data[0].avgFoul * 100).toFixed(0) + "%";
+        document.getElementById("minFoul").innerHTML = (data[0].minFoul * 100).toFixed(0) + "%";
+        document.getElementById("maxFoul").innerHTML = (data[0].maxFoul * 100).toFixed(0) + "%";
+    }, "json");
 }
